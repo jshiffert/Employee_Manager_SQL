a058b14bbedb88d60132e71d329a057ae0c6766c
jshiffert <jshiffert@oki.org>
2023-07-26 13:12:00 -0400

    fully functional table logs

--- server.js
+++ server.js
@@ -20,28 +20,67 @@ const db = mysql.createConnection(
     console.log(`Connected to the employee_db database.`)
 );
 
-function print_table(results) {
-    const p = new Table();
-    p.addRows(results);
-    const newTable = p.render();
-    console.log(`\n${newTable}\n\n`+("\n"*results.length));
-}
 
 function all_deps() {
     db.query('SELECT * FROM department', function (err, results) {
-        print_table(results);
+        const p = new Table({
+            columns: [
+                {name: "id"},
+                {name: "dep_name",
+                title: "department name"}
+            ]
+        });
+        p.addRows(results);
+        const newTable = p.render();
+        console.log(`${newTable}`);
     });
 };
 
 function all_role() {
-    db.query('SELECT * FROM e_role', function (err, results) {
-        print_table(results);
+    db.query('SELECT e_role.id, e_role.title, e_role.salary, department.dep_name '
+            + 'FROM e_role INNER JOIN department '
+            + 'ON e_role.department_id=department.id', function (err, results) {
+        const p = new Table({
+            columns: [
+                {name: "id"},
+                {name: "title"},
+                {name: "dep_name",
+                title: "department"},
+                {name: "salary"}
+            ]
+        });
+        p.addRows(results);
+        const newTable = p.render();
+        console.log(`${newTable}`);
     });
 };
 
 function all_employee() {
-    db.query('SELECT * FROM employee', function (err, results) {
-        print_table(results);
+    db.query('SELECT a.id, a.first_name, a.last_name, e_role.title, '
+            + 'department.dep_name, e_role.salary, '
+            + 'CONCAT(b.first_name, " ", b.last_name) AS manager '
+            + 'FROM employee a '
+            + 'INNER JOIN e_role ON a.role_id=e_role.id '
+            + 'INNER JOIN department ON e_role.department_id=department.id '
+            + 'LEFT JOIN employee b ON b.id = a.manager_id'
+            , function (err, results) {
+                const p = new Table({
+                    columns: [
+                        {name: "id"},
+                        {name: "first_name",
+                        title: "first name"},
+                        {name: "last_name",
+                        title: "last name"},
+                        {name: "title"},
+                        {name: "dep_name",
+                        title: "department"},
+                        {name: "salary"},
+                        {name: "manager"}
+                    ]
+                });
+                p.addRows(results);
+                const newTable = p.render();
+                console.log(`${newTable}`);
     });
 };
 
@@ -56,7 +95,23 @@ app.listen(PORT, () => {
 
 console.log("\r\n                                                 *                                          \r\n                     (                         (  `                                         \r\n (       )           )\\      (       (    (    )\\))(      )            )  (  (     (   (    \r\n )\\     (     `  )  ((_) (   )\\ )   ))\\  ))\\  ((_)()\\  ( \/(   (     ( \/(  )\\))(   ))\\  )(   \r\n((_)    )\\  \' \/(\/(   _   )\\ (()\/(  \/((_)\/((_) (_()((_) )(_))  )\\ )  )(_))((_))\\  \/((_)(()\\  \r\n| __| _((_)) ((_)_\\ | | ((_) )(_))(_)) (_))   |  \\\/  |((_)_  _(_\/( ((_)_  (()(_)(_))   ((_) \r\n| _| | \'  \\()| \'_ \\)| |\/ _ \\| || |\/ -_)\/ -_)  | |\\\/| |\/ _` || \' \\))\/ _` |\/ _` | \/ -_) | \'_| \r\n|___||_|_|_| | .__\/ |_|\\___\/ \\_, |\\___|\\___|  |_|  |_|\\__,_||_||_| \\__,_|\\__, | \\___| |_|   \r\n             |_|             |__\/                                        |___\/              \r\n");
 
-const prompt1 = () => {
+// Credit:
+// https://stackoverflow.com/questions/63161758/text-in-bash-terminal-getting-overwritten-using-js-node-js-npms-are-inquirer
+function restart() {
+    var uScore = "_";
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(uScore.repeat(50));
+        }, 1000);
+    });
+}
+
+
+const prompt1 = async () => {
+    await restart()
+    .then((lineBreak) => {
+        console.log(`${lineBreak}\n`);
+    });
     const startQ = [
         {
             type: 'list',
@@ -71,11 +126,12 @@ const prompt1 = () => {
     return inquirer.prompt(startQ);
 }
 
+// Credit:
+// https://stackoverflow.com/questions/61417816/how-do-i-invoke-inquirer-js-menu-in-a-loop-using-promises
 const main = async () => {
     for (let x=0; x < 10; x++) {
         await prompt1() 
         .then((answer) => {
-            console.log(answer.start+'\n');
             if (answer.start == "View all departments") {
                 all_deps();
             } else if (answer.start == "View all roles") {
